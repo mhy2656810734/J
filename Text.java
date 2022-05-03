6929ab9a4f4c2788dbce23987b5221c15b045d42
mhy2656810734 <2656810734@qq.com>
2022-05-03 11:51:28 +0800

    String[]agrs的作用

---
+++ Text.java
@@ -0,0 +1,12 @@
+public class Text {
+	public static void main(String[] args){
+		int i=0;
+		for(i=0;i<args.length;i++)
+			{
+				//args的作用：运行时命令行参数，只有运行时可以打印
+				System.out.println(args[i]);
+
+			}
+			System.out.println("HelloWorld");
+	}
+}
