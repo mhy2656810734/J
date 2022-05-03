b74a0f52fff634c9af608da2e751695dd0643a69
mhy2656810734 <2656810734@qq.com>
2022-05-03 09:56:53 +0800

    Long长整型最大最小值输入输出

---
+++ 最大最小值输出/MAX.java
@@ -0,0 +1,10 @@
+public class MAX {
+	public static void main(String[] args){
+		long a=10L;
+		long b=Long.MAX_VALUE;
+		long c=Long.MIN_VALUE;
+		System.out.println("a的值是：" + a);
+		System.out.println("最大值是:" + b);
+		System.out.println("最小值是:" + c);
+	}
+}
