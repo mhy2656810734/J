d0a113f2e2a0361e235cc2e3a879ad8a55be26c4
mhy2656810734 <2656810734@qq.com>
2022-05-04 11:22:41 +0800

    转义字符以及字符串拼接

---
+++ 转义字符以及字符串拼接/text.java
@@ -0,0 +1,11 @@
+public class text {
+	public static void main(String[] args) {
+		String a="\'hello\'";
+
+		String b="\"world\"";
+
+		System.out.println(a+b);
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
