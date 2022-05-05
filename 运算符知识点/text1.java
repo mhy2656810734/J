62a5c07b17a2c8582bb34e7d9971c7706dc980ac
mhy2656810734 <2656810734@qq.com>
2022-05-05 08:47:33 +0800

    6种关系运算符

---
+++ 运算符知识点/text1.java
@@ -0,0 +1,17 @@
+public class text1 {
+	/**
+	 * 6种关系运算符
+	 *   ==  !=  >  <  <=  >=
+	 */
+	public static void main(String[] args){
+      
+      int a=10;
+      int b=20;
+      System.out.println(a==b);
+      System.out.println(a!=b);
+      System.out.println(a<b);
+      System.out.println(a>b);
+      System.out.println(a<=b);
+      System.out.println(a>=b);
+	}
+}
