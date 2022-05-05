a033809569e2a77f85fcfcac8fcf9bc1a79fe011
mhy2656810734 <2656810734@qq.com>
2022-05-05 13:10:15 +0800

    一些练习题

---
+++ 练习题/text.java
@@ -0,0 +1,24 @@
+public class text {
+	/**
+	 * 交换两个变量的值
+	 */
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		a=a^b;
+		b=a^b;
+		a=a^b;
+		// a=a+b;
+		// b=a-b;
+		// a=a-b;
+
+		// int c=0;
+		// c=a;
+		// a=b;
+		// b=c;
+			System.out.println(a);
+		System.out.println(b);
+
+	}
+
+	}
