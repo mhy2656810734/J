54a28cc56b9774c86981009089eaac49229a141d
mhy2656810734 <2656810734@qq.com>
2022-05-05 09:18:32 +0800

    四种位运算符 & | ~ ^

---
+++ 运算符知识点/text3.java
@@ -0,0 +1,14 @@
+public class text3 {
+	/**
+	 * 四个位运算符
+	 * & | ~ ^ 
+	 */
+	public static void main(String[] args){
+		int a=1;
+		int b=2;
+		System.out.println((a==1)&(b==3));
+		System.out.println((a==1)|(b==3));
+		System.out.println(~a);
+		System.out.println(a^b);
+	}
+}
