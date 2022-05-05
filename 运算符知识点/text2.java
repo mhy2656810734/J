79a3e87229f9755d5a60d6f74da57cf884b35c13
mhy2656810734 <2656810734@qq.com>
2022-05-05 09:06:20 +0800

    三个逻辑运算符 &&  ||  ！

---
+++ 运算符知识点/text2.java
@@ -0,0 +1,15 @@
+public class text2 {
+	/**
+	 * 三个逻辑运算符
+	 * 逻辑运算符的操作数和返回值都是boolean
+	 */
+	public static void main(String[] args){
+		int a=10;
+		int b=20;
+		int c=30;
+		boolean flag=true;
+		System.out.println(!flag);//  ！运算符只能作用到布尔类型的数据上
+		System.out.println((a<b)&&(b<c));//true
+
+	}
+}
