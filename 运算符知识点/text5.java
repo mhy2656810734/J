9b3d249091b2da46396a6102108c1bad61b93280
mhy2656810734 <2656810734@qq.com>
2022-05-05 09:54:08 +0800

    输入数字(字符串)输出数字(字符串)

---
+++ 运算符知识点/text5.java
@@ -0,0 +1,19 @@
+// public class text4 {
+// 	public static void main(String[] args) {
+// 		int a=1;
+// 		int b=2;
+// 		System.out.println((a==1)&&(b==2));
+// 	}
+// }
+import java.util.Scanner;
+public class text4 {
+	public static void main(String[] args) {
+		Scanner scan =new Scanner(System.in);//输入数字输出数字
+		// int a =scan.nextInt();
+		// String str=scan.nextLine();//输入一行字符串,遇到空格不停止
+		// String str =scan.next();//遇到空格停止
+		String str = scan.nextLine();
+		// System.out.println(a);
+		System.out.println(str);
+	}
+}
