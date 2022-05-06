39a75c3c271980a5dae92689a3dc2165ea024ba9
mhy2656810734 <2656810734@qq.com>
2022-05-06 13:10:25 +0800

    判断素数/猜数字游戏

---
+++ 练习题/判断是否为素数/text.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class text {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int i=0;
+		int flag=0;
+		for(i=2;i<n;i++){
+			if(n%i==0){
+				System.out.println(n+"不是素数");
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0)
+		{
+			System.out.println(n+"是素数");
+		}
+		
+	}
+}
