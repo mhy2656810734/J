da5c2acbc4f7740c50401aeca6ea2a0b160e0112
mhy2656810734 <2656810734@qq.com>
2022-05-06 11:34:55 +0800

    for循环

---
+++ java四种循环/for/text.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class text {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int i=0;
+		for(;i<n;i++){
+			System.out.println("海洋 is cool");
+		}
+	}
+}
