41f34ae18d9ea365d7f508271e01062ee1ac5b6c
mhy2656810734 <2656810734@qq.com>
2022-05-06 13:47:58 +0800

    打印1000到2000之间的闰年

---
+++ 练习题/打印1000到2000之间的闰年/text.java
@@ -0,0 +1,10 @@
+public class text {
+	public static void main(String[] args){
+		int i=0;
+		for(i=1000;i<=2000;i++){
+			if((i%4==0 && i%100!=0)||i%400==0){
+				System.out.println(i+"是闰年");
+			}
+			}
+	} 
+}s
