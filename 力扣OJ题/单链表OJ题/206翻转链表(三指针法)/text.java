11e8fbedb4f0960b9ff80777c45434fe11fb9fe3
mhy2656810734 <101716458+mhy2656810734@users.noreply.github.com>
2022-09-17 10:17:03 +0800

    Update text.java

--- 力扣OJ题/单链表OJ题/206翻转链表(三指针法)/text.java
+++ 力扣OJ题/单链表OJ题/206翻转链表(三指针法)/text.java
@@ -8,21 +8,34 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ // 核心思路: 三指针法
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode cur =  head;
-        ListNode newHead = null;// 要返回的最终的新头节点
-         ListNode curNext = null;
+ if (head == null) {
+            return null;
+        }
+        // ListNode prev = null;
+        // ListNode cur = head;
+        // ListNode newHead = null;
+        // while (cur != null) {
+        //     ListNode curNext = cur.next;
+        //     // 判断curNext是不是已经为空,为空就是新头结点
+        //     if (curNext == null) {
+        //         newHead = cur;
+        //     } 
+        //     cur.next = prev;
+        //     prev = cur;
+        //     cur = curNext;
+        // }
+        // return newHead;
+        ListNode cur = head.next;
+        head.next = null;
         while (cur != null) {
-            curNext = cur.next;
-            if (curNext == null) {
-                newHead = cur;
-            }
-            cur.next = prev;
-            prev = cur;
+            ListNode curNext = cur.next;
+            cur.next = head;
+            head = cur;
             cur = curNext;
         }
-        return newHead;
-    }
+        return head;
+}
 }
