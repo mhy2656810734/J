94e4f2437516d0f0904b2f68d1487540b0ff12f9
mhy2656810734 <101716458+mhy2656810734@users.noreply.github.com>
2022-09-19 09:42:32 +0800

    Update text.java

--- 力扣OJ题/单链表OJ题/回文链表/text.java
+++ 力扣OJ题/单链表OJ题/回文链表/text.java
@@ -12,45 +12,43 @@
  // 例:1->2->3->2->1->null  --->>    1->2->3<-2-<1
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        // 链表为空
-        if (head == null) {
+       if (head == null) {
             return false;
         }
-        // 链表为一个结点
+        // 只有一个节点的情况下
         if (head.next == null) {
             return true;
         }
-        // 1.先找链表的中间节点 (快慢指针)
+        // 1.先找中间节点 快慢指针
         ListNode fast = head;
         ListNode slow = head;
-        while ((fast !=null)&&(fast.next != null)){
+        while ((fast != null)&&(fast.next != null)) {
             fast = fast.next.next;
             slow = slow.next;
         }
-        // 此时slow就是中间结点 
-        // 2.反转后半段结点
-        ListNode cur = slow.next;
+        // 2.翻转后半段链表 三指针法
+        ListNode prev = slow;
+        ListNode cur = prev.next;
         while (cur != null) {
-            ListNode curNext = cur.next; // 反转链表必须需要三指针
-            cur.next = slow;
-            slow = cur;
-            cur = curNext;
+            ListNode curNext = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur =curNext;
         }
-        // 此时反转链表成功,slow就是最后一个节点
-        // 3.判断是否为回文链表:head 和 slow 一次走一步，直到相等结束
-        while (slow != head) {
-            // 不相等就不是
-            if (slow.val != head.val) {
+        // 3.判断
+        while (head != prev) {
+            // 不相等
+            if (head.val != prev.val) {
                 return false;
             }
-            // 判断偶数的情况
-            if (head.next == slow) {
+            // 偶数节点的情况
+            if (head.next == prev) {
                 return true;
             }
             head = head.next;
-            slow = slow.next;
+            prev = prev.next;
         }
-        // 循环结束 说明是
+        // 相等
         return true;
     }
 }
